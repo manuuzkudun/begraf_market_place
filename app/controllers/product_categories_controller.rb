941582dc1315e86d18f8de600c28142f548b9d66
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-21 18:28:00 +0100

    Dynamic form implemented

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -4,6 +4,10 @@ class ProductCategoriesController < ApplicationController
   end
 
   def show
-
+    @product_catecory = ProductCategory.find(params[:id])
+    @product_properties = @product_catecory.product_properties
+    respond_to do |format|
+      format.js
+    end
   end
 end
