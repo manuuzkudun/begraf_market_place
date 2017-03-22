e358569f25c226ac04df97fa22046879763d3bfb
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-22 21:47:33 +0100

    Product seed updated

--- db/product_seeds.rb
+++ db/product_seeds.rb
@@ -1,32 +1,4 @@
 require_relative 'business_cards'
-require_relative 'flyers'
-
-[@business_cards_data, @flyers_data].each do |product_category|
-
-  category = ProductCategory.create name: product_category[:name],
-    image_url: product_category[:image_url], question: product_category[:question]
-
-  product_category[:properties].each do |property_data|
-    property = ProductProperty.new name: property_data[:name],
-    question: property_data[:question], show: property_data[:show]
-    property.product_category = category
-    property.save
-    property_data[:values].each do |value_data|
-      prop_value = PropertyValue.new value: value_data[:value], image_url: value_data[:image_url]
-      prop_value.product_property = property
-      prop_value.save
-    end
-  end
-end
-
-business_cards = ProductCategory.first
-
-shop1 = Shop.create name: 'Cool shop', email: 'cool-shop@domain.com', address: 'test address',
-  telephone: '697697680', nif: 'jyfjfyyu'
-
-shop2 = Shop.create name: 'Lisbon print', email: 'lisbon-print@domain.com', address: 'test 2 address',
-  telephone: '698897680', nif: 'jiiifjfyyu'
-
 
 def get_combinations(product_category)
   combinations = ['']
@@ -72,8 +44,5 @@ Product.all.each do |product|
     ShopProduct.create product: product, shop: shop, avalaible: true, delivery_days: rand(7), price: rand(200)
   end
 end
-
-
-
 
 
