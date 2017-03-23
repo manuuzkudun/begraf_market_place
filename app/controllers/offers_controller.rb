8dc6c7bfaeecfacc89938f4b7314cae085bcccaa
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-23 20:15:11 +0100

    Selected product info showing in the offers section

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,7 @@
 class OffersController < ApplicationController
   def find_offers
-
-        puts "----------"
-        pp product_params
-        puts "----------"
-    product = Product.where(product_params)
-    @offers = ShopProduct.where(product: product)
+    @product = Product.where(product_params).first
+    @offers = ShopProduct.where(product: @product)
     respond_to do |format|
       format.js
     end
