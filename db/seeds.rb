a1a2b033992bf236da590a2d5d31a43c2dc6baaf
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-25 17:19:45 +0100

    OrderStatus added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,6 +56,11 @@ Product.all.each do |product|
 end
 
 
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
+
 
 
 
