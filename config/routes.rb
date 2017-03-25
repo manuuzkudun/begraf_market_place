ba7fce8cf5c9a46c9773a80188eab6b1b7e47081
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-25 17:48:39 +0100

    current method added to AplicationController

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
 
-  get 'carts/show'
 
   devise_for :users
 
   resources :order_products, only: [:create, :index, :destroy]
   resources :product_categories, only: [:index, :show]
+  resource :cart, only: [:show]
   post '/offers', to: "offers#find_offers"
   root to: 'product_categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
