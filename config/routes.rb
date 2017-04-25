97f01af6d99786ac2b399127eea6ffb5941f2e7d
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-04-25 19:11:04 +0200

    home page basic implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
   resources :product_categories, only: [:index, :show]
   resources :offers, only: [:index]
@@ -7,5 +6,5 @@ Rails.application.routes.draw do
   resources :order_products, only: [:create, :destroy]
   resource :cart, only: [:show]
   resources :payments, only: [:new, :create, :show]
-  root to: 'product_categories#index'
+  root to: 'pages#home'
 end
